e777b6fbc3ec5fc4766949ccb32d8d3166fc1dff
Shannon M. Denna <sdenna@d211.org>
2025-01-30 09:48:36 -0600

    Removed unnecessary styles from ListItem

--- flatlistApp/components/ListItem.tsx
+++ flatlistApp/components/ListItem.tsx
@@ -10,18 +10,4 @@ const ListItem = () => {
 
 export default ListItem;
 
-const styles = StyleSheet.create({
-  list: {
-    backgroundColor: colors.secondary,
-    padding: 5,
-    height: 60,
-    width: 350,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    marginTop: 5,
-  },
-  text: {
-    color: colors.text.dark,
-    fontSize: 20,
-  },
-});
+const styles = StyleSheet.create({});
